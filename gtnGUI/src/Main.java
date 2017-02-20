50dbb3f9e0fa5fe60c214044a4e0ca61f08afe5e
Heron Navarro <Heron Navarro>
2017-02-20 13:21:31 -0800

    Almost finished GUI//Needs working quit buttons

--- gtnGUI/src/Main.java
+++ gtnGUI/src/Main.java
@@ -13,25 +13,26 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import jdk.nashorn.internal.runtime.regexp.joni.constants.StackPopLevel;
-
 import java.util.Random;
 
 public class Main extends Application {
 
     Stage window;
-    Scene menu, game1;
-    Button button;
-    Button gameButton;
-    int lives = 4;
+    Scene menu, game1, wonGame, lostGame;
+    Button button, gameButton, restart, quit;
+    int guesses = 0;
+    int chosen;
     int userGuess;
+    String endMsg;
+    String againTxt = "Let's play Again!";
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        window =primaryStage;
+        window = primaryStage;
         window.setTitle("Welcome to Guess the Number");
 
         Label welcome = new Label("Welcome to Guess the Number!!");
@@ -40,11 +41,14 @@ public class Main extends Application {
 
         button = new Button("Click Me");
         button.setOnAction(e -> {
+            Random rand = new Random();
+            chosen = rand.nextInt(51);
+            System.out.println("The number is " + chosen + " Guesses equals zero");
             window.setScene(game1);
         });
 
         VBox menu1 = new VBox(150);
-        menu1.getChildren().addAll(welcome,start);
+        menu1.getChildren().addAll(welcome, start);
 
         StackPane menu2 = new StackPane();
         menu2.getChildren().add(button);
@@ -53,7 +57,7 @@ public class Main extends Application {
         grandeScheme.setTop(menu1);
         grandeScheme.setCenter(menu2);
 
-        menu = new Scene(grandeScheme,450,400);
+        menu = new Scene(grandeScheme, 450, 400);
         menu1.setAlignment(Pos.CENTER);
 
 
@@ -61,42 +65,114 @@ public class Main extends Application {
         Label gameText = new Label("Enter a number!");
         TextField userIn = new TextField();
         gameButton = new Button("Submit");
-        gameButton.setOnAction(e ->{
-            Random rand = new Random();
-            int chosen = rand.nextInt(51);
-            userGuess = Integer.valueOf(userIn.getText());
-            userIn.clear();
-            String numberComp = compInt(userGuess, chosen);
-            String checkWin = winCheck(numberComp, chosen, userGuess);
+        gameButton.setOnAction(e -> {
+            if (guesses <=4) {
+                try {
+                    int userGuess = Integer.parseInt(userIn.getText());
+                    guesses++;
+                    String numberComp = compInt(userGuess, chosen);
+                    userIn.clear();
+                    System.out.println(numberComp);
+
+                    if (numberComp.contains("Winner!")) {
+                        guesses = 0;
+                        window.setScene(wonGame);
+                    } else if (guesses == 5 && numberComp.contains("Winner!")){
+                        guesses = 0;
+                        window.setScene(wonGame);
+                    } else if(guesses==5 && !numberComp.contains("Winner!")) {
+                        guesses = 0;
+                        window.setScene(lostGame);
+                    }
+                } catch (NumberFormatException exception) {
+                    System.out.println("Please enter integer!! I wont count that as a guess");
+                    userIn.clear();
+                }
+            }
         });
 
         VBox layout2 = new VBox(20);
-        layout2.getChildren().addAll(gameText,userIn,gameButton);
+        layout2.getChildren().addAll(gameText, userIn, gameButton);
         layout2.setAlignment(Pos.CENTER);
-        game1 = new Scene(layout2,450,400);
+        game1 = new Scene(layout2, 450, 400);
 
+        //Scene 3 - wonGame
+        Label resultTxt = new Label("You Won Congratulations!!!");
+        Label again = new Label(againTxt);
+
+        //wonGame buttons(s)
+        restart = new Button("I'm Cool and I want to try again");
+        restart.setOnAction(e -> {
+            window.setScene(menu);
+        });
+        quit = new Button("Nah, I'm Lame and just want to go...");
+
+        //wonGame layout
+        //Text in layout will be vertical and centered messages
+        VBox layout3 = new VBox(20);
+        layout3.getChildren().addAll(resultTxt,again);
+        layout3.setAlignment(Pos.CENTER);
+
+        //Buttons in layout will be horizontal and centered
+        HBox l3Btns = new HBox(20);
+        l3Btns.getChildren().addAll(restart,quit);
+        l3Btns.setAlignment(Pos.CENTER);
+
+        //combine layouts
+        BorderPane grandeScheme2 = new BorderPane();
+        grandeScheme2.setTop(layout3);
+        grandeScheme2.setCenter(l3Btns);
+
+        wonGame = new Scene(grandeScheme2, 450,450);
+
+
+        //Scene 4 - lostGame
+        Label resultTxt2 = new Label("I'm Sorry but you failed to guess the Correct number..");
+        Label DGU = new Label("But don't give upp I believe in you!!");
+
+        //lostGame buttons(s)
+        restart = new Button("I'm Cool and I want to try again");
+        restart.setOnAction(e -> {
+            window.setScene(menu);
+        });
+
+        //lostGame layout
+        //Text in layout will be vertical and centered messages
+        VBox layout4 = new VBox(20);
+        layout4.getChildren().addAll(resultTxt2,DGU);
+        layout4.setAlignment(Pos.CENTER);
+
+        //Buttons in layout will be horizontal and centered
+        HBox l4Btns = new HBox(20);
+        l4Btns.getChildren().addAll(restart,quit);
+        l4Btns.setAlignment(Pos.CENTER);
+
+        //combine layouts
+        BorderPane grandeScheme3 = new BorderPane();
+        grandeScheme3.setTop(layout4);
+        grandeScheme3.setCenter(l4Btns);
+
+        lostGame = new Scene(grandeScheme3, 450,450);
 
         window.setScene(menu);
         window.show();
 
     }
 
-    static String compInt(int userGuess, int chosen){
+    static String compInt(int userGuess, int chosen) {
 
         String numberComp;
         numberComp = "";
 
-        if(userGuess==chosen){
-            numberComp="Winner!";
-        }
-        else if (userGuess > chosen) {
-            if (userGuess > chosen && userGuess> 50) {
+        if (userGuess == chosen) {
+            numberComp = "Winner!";
+        } else if (userGuess > chosen) {
+            if (userGuess > chosen && userGuess > 50) {
                 numberComp = "not even in range, come on... pick a number between 0 and 50";
             } else {
                 numberComp = " too large";
             }
-        }
-        else if (userGuess < chosen) {
+        } else if (userGuess < chosen) {
             if (userGuess < chosen && userGuess < 0) {
                 numberComp = "not even in range, come on... pick a number between 0 and 50";
             } else {
@@ -106,30 +182,5 @@ public class Main extends Application {
 
         return numberComp;
     }
-    public String winCheck(String numberComp, int chosen, int userGuess){
-        String checkWin = "";
-        for(lives = 4; lives>=0; lives-=1) {
-            if(lives == 0 && numberComp.equals("Winner!")){
-                checkWin = "Won on fifth try";
-                return checkWin;
-
-            }
-            else if(lives ==0 && !numberComp.equals("Winner")){
-                checkWin = "loser";
-                return checkWin;
-            }
-            else {
-                if (numberComp.equals("Winner!")) {;
-                    checkWin = "Winner";
-                } else {
-
-                    checkWin = "Sorry, but that answer was" + numberComp + " you have " + lives + " lives left.";
-                    numberComp = compInt(userGuess, chosen);
-                }
-            }
-        }
-        return checkWin;
-    }
-
-    }
+}
 
